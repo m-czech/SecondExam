dc8e3aaf72eabd0540ec24be3f331ed9b307ca30
matt <felvi00@gmail.com>
2022-08-08 19:10:53 +0200

    Implement profiles for Automapper

---
+++ SecondExam/Profiles/EducationalMaterialProfile.cs
@@ -0,0 +1,14 @@
+﻿using AutoMapper;
+using SecondExam.DTOs.EducationalMaterial;
+using SecondExam.Entities;
+
+namespace SecondExam.Profiles;
+
+public class EducationalMaterialProfile : Profile
+{
+    public EducationalMaterialProfile()
+    {
+        CreateMap<EducationalMaterial, CreateEducationalMaterialDto>().ReverseMap();
+        CreateMap<EducationalMaterial, GetEducationalMaterialDto>().ReverseMap();
+    }
+}
