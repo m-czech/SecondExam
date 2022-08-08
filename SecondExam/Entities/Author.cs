eb3507a7d108df408170079d63746a9dd04ccd31
matt <felvi00@gmail.com>
2022-08-08 11:40:26 +0200

    Add Author entity

--- SecondExam/Entities/Author.cs
+++ SecondExam/Entities/Author.cs
@@ -1,6 +1,10 @@
-﻿namespace SecondExam.Entities
+﻿namespace SecondExam.Entities;
+
+public class Author
 {
-    public class Author
-    {
-    }
+    public int Id { get; set; }
+    public string? Name { get; set; }
+    public string? Description { get; set; }
+    public IEnumerable<EducationalMaterial>? Materials { get; set; }
+    public int? CreatedMaterials { get; set; }
 }
--- SecondExam/Entities/EducationalMaterial.cs
+++ SecondExam/Entities/EducationalMaterial.cs
@@ -1,13 +1,13 @@
-﻿namespace SecondExam.Entities
+﻿namespace SecondExam.Entities;
+
+public class EducationalMaterial
 {
-    public class EducationalMaterial
-    {
-        public Author? Athor { get; set; }
-        public string? Title { get; set; }
-        public string? Description { get; set; }
-        public string? Location { get; set; }
-        public EducationalMaterialType? Type { get; set; }
-        IEnumerable<EducationalMaterialReview>? Reviews { get; set; }
-        DateTime? PublishingDate { get; set; }
-    }
+    public int Id { get; set; }
+    public Author? Author { get; set; }
+    public string? Title { get; set; }
+    public string? Description { get; set; }
+    public string? Location { get; set; }
+    public EducationalMaterialType? Type { get; set; }
+    IEnumerable<EducationalMaterialReview>? Reviews { get; set; }
+    DateTime? PublishingDate { get; set; }
 }
