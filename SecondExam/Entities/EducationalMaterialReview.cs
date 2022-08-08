7b1a800a24d2d5142b9221393ecd8ad085ab0e3f
matt <felvi00@gmail.com>
2022-08-08 11:42:35 +0200

    Add Review entity

--- SecondExam/Entities/EducationalMaterialReview.cs
+++ SecondExam/Entities/EducationalMaterialReview.cs
@@ -1,6 +1,9 @@
-﻿namespace SecondExam.Entities
+﻿namespace SecondExam.Entities;
+
+public class EducationalMaterialReview
 {
-    public class EducationalMaterialReview
-    {
-    }
+    public int Id { get; set; }
+    public EducationalMaterial? ReviewedMaterial { get; set; }
+    public string? TextReview { get; set; }
+    public int? DigitReview { get; set; }
 }
