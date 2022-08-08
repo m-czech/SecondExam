eb1b9d4f65abf2f054b8ae4037e6d51f41aaf4a7
matt <felvi00@gmail.com>
2022-08-08 12:05:49 +0200

    Implement database context for ef core

---
+++ SecondExam/Repository/RepositoryContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SecondExam.Entities;
+
+namespace SecondExam.Repository;
+
+public class RepositoryContext : DbContext
+{
+    public RepositoryContext(DbContextOptions options) : base(options)
+    { }
+
+    public DbSet<EducationalMaterial>? EducationalMaterials { get; set; }
+    public DbSet<Author>? Authors { get; set; }
+    public DbSet<EducationalMaterialReview>? Reviews { get; set; }
+    public DbSet<EducationalMaterialType>? EducationalMaterialTypes { get; set; }
+}
