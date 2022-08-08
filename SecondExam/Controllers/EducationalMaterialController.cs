e7d6c69df2eeba3bf4819873e1d881b1094d1c36
matt <felvi00@gmail.com>
2022-08-08 13:46:47 +0200

    Implement EducationalMaterialController startup code

---
+++ SecondExam/Controllers/EducationalMaterialController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SecondExam.Repository.Contracts;
+
+namespace SecondExam.Controllers;
+
+[ApiController]
+[Route("api/material")]
+public class EducationalMaterialController : ControllerBase
+{
+    private readonly IRepositoryManager _repositories;
+    public EducationalMaterialController(IRepositoryManager repositories)
+    {
+        _repositories = repositories;
+    }
+
+}
